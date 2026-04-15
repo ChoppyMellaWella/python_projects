3dca04d5c0699b1ffbe41ba37dee53de308e5e37
Gabriel Somera <gabrielsomera06@gmail.com>
2026-04-14 16:26:16 -1000

    Update variable names and license information; add .gitignore for Python and macOS

--- Day04/string_notes.py
+++ Day04/string_notes.py
@@ -5,7 +5,7 @@ just some notes on day 4 of all about strings
 """
 
 # first time with multi-line strings
-intro = """Hi all. My name is Gabriel and I like computer networking, linux, programming(right now), and everything IT/Computers!
+intro = """Hi all. My name is gabuki and I like computer networking, linux, programming(right now), and everything IT/Computers!
 
 Still a fledgling in the world of IT. I'll be making my mark in the near future- wait and see!
 
@@ -15,7 +15,7 @@ print(intro)
 
 nickname = 'gabuki'
 
-intro_test = f"""Hi all. My name is Gabriel and I like computer networking, linux, programming(right now), and everything IT/Computers!
+intro_test = f"""Hi all. My name is gabuki and I like computer networking, linux, programming(right now), and everything IT/Computers!
 
 Still a fledgling in the world of IT. I'll be making my mark in the near future- wait and see!
 
