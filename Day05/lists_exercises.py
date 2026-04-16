e583131032ffaaabdf1b29ac4d10064946b19c04
Gabuki <gabrielsomera06@gmail.com>
2026-04-15 23:14:29 -1000

    Add list manipulation exercises and additional functionality to lists_exercises.py

--- Day05/lists_exercises.py
+++ Day05/lists_exercises.py
@@ -66,3 +66,296 @@ it_companies.reverse()
 print(it_companies)
 
 # 18. Slice out the first 3 companies from the list
+print(it_companies[:3:])
+
+# 19. Slice out the last 3 companies from the list
+print(it_companies[-1:-4:-1]) # reversed
+print(it_companies[len(it_companies)-3:]) # last 3 left-to-right
+
+# 20. Slice out the middle IT company or companies from the list
+print(it_companies[len(it_companies)//2])
+
+# 21. Remove the first IT company from the list
+del it_companies[0]
+print(it_companies)
+
+# 22. Remove the middle IT company or companies from the list
+del it_companies[(len(it_companies)-1)//2]
+print(it_companies)
+
+# 23. Remove the last IT company from the list
+del it_companies[-1]
+print(it_companies)
+
+# 24. Remove all IT companies from the list
+it_companies.clear()
+print(it_companies)
+
+# 25. Destroy the IT companies list
+del (it_companies)
+
+"""
+26. Join the following lists:
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+
+"""
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+
+front_end.extend(back_end)
+print(front_end)
+
+# 27. After joining the lists in question 26. Copy the joined list and 
+# assign it to a variable full_stack, then insert Python and SQL after Redux.
+
+full_stack = front_end
+full_stack.insert(full_stack.index('Node'),'Python')
+full_stack.insert(full_stack.index('Node'),'SQL')
+print(full_stack)
+
+# LEVEL 2
+
+"""
+1. 
+    The following is a list of 10 students ages:
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+    Sort the list and find the min and max age
+    Add the min age and the max age again to the list
+    Find the median age (one middle item or two middle items divided by two)
+    Find the average age (sum of all items divided by their number )
+    Find the range of the ages (max minus min)
+    Compare the value of (min - average) and (max - average), use abs() method
+
+"""
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+print(ages)
+ages.sort()
+print(f"sorted ages: {ages}")
+print(f"min age: {ages[0]}\nmax age: {ages[-1]}")
+print(f"median age: {ages[len(ages)//2]}")
+print(f"average age: {sum(ages)//len(ages)}") # sum() not part of course
+print(f"range age: {ages[-1] - ages[0]}")
+print(f"(min - average): {-(ages[0] - (sum(ages)//len(ages)))}\n(max - average): {ages[-1] - (sum(ages)//len(ages))}") # added - as the suffix for min - average
+
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cabo Verde',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombia',
+  'Comoros',
+  'Congo, Democratic Republic of the',
+  'Congo, Republic of the',
+  'Costa Rica',
+  "Côte d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor-Leste)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Eswatini',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Montenegro',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'North Macedonia',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Palestine',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent and the Grenadines',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'South Sudan',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe'
+];
+
+"""
+1. Find the middle country(ies) in the countries list
+2. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+3. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+
+"""
+
+# 1. Find the middle country(ies) in the countries list
+print(countries[len(countries)//2], countries[(len(countries)//2)-1])
+
+# 2. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+first_half, second_half = countries[:len(countries)//2], countries[len(countries)//2]
+print(first_half)
+print()
+print(second_half)
+# 3. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+these_countries = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+first, second, third, *scandic = these_countries
+print(first, second, third, scandic)
