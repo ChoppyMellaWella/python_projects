268d0bff87fb059b06d317a76f5b01c4299ff7a6
Gabuki <gabrielsomera06@gmail.com>
2026-04-21 18:34:16 -1000

    Add conditional exercises for grading, seasons, fruit management, and person skills

--- Day09/conditionals_exercises.py
+++ Day09/conditionals_exercises.py
@@ -70,4 +70,115 @@ else:
 
 user_grade = int(input('what was your grade for one class: '))
 
+match user_grade:
+    case user_grade if user_grade >= 90 and user_grade <= 100:
+        print('you have an A in that class')
+    case user_grade if user_grade >= 80 and user_grade <= 89:
+        print('you have a B in that class')
+    case user_grade if user_grade >= 70 and user_grade <= 79:
+        print('you have a C in that class')
+    case user_grade if user_grade >= 60 and user_grade <= 69:
+        print('you have a D in that class')
+    case user_grade if user_grade >= 0 and user_grade <= 59:
+        print('you have an F in that class')
+    case _:
+        print('invalid grade option')
 
+
+"""
+Get the month from user input then check if the season is Autumn, Winter, 
+Spring or Summer. If the user input is: September, October or November, the season is 
+Autumn. December, January or February, the season is Winter. March, April or May, 
+the season is Spring June, July or August, the season is Summer
+"""
+
+user_month = input('what is your favorite month?: ').lower().replace(" ", "")
+
+match user_month:
+    case user_month if user_month.find('september') != -1 or user_month.find('october') != -1 or user_month.find('november') != -1:
+        print('the season is autumn')
+    case user_month if user_month.find('december') != -1 or user_month.find('january') != -1 or user_month.find('february') != -1:
+        print('the season is winter')
+    case user_month if user_month.find('march') != -1 or user_month.find('april') != -1 or user_month.find('may') != -1:
+        print('the season is spring')
+    case user_month if user_month.find('june') != -1 or user_month.find('july') != -1 or user_month.find('august') != -1:
+        print('the season is summer')
+    case _:
+        print('type the correct month')
+
+"""
+    The following list contains some fruits:
+
+```sh
+fruits = ['banana', 'orange', 'mango', 'lemon']
+```
+
+If a fruit doesn't exist in the list add the fruit to the list and print the modified list. If the fruit exists print('That fruit already exist in the list')
+
+"""
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+
+print(fruits)
+
+user_fruit = input('whats a fruit that you want added to our list?: ').lower().replace(" ", "")
+
+if user_fruit in fruits:
+    print(f'try again, there already is {user_fruit} in there')
+else:
+    fruits.append(user_fruit)
+    print(fruits)
+
+"""
+        person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
+
+ * Check if the person dictionary has skills key, if so print out the middle skill in the skills list.
+ 
+ * Check if the person dictionary has skills key, if so check if the person has 'Python' skill and print out the result.
+ 
+ * If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, 
+   Python, MongoDB, print('He is a backend developer'), if the person skills has React, Node and MongoDB, 
+   Print('He is a fullstack developer'), else print('unknown title') - for more accurate results more conditions can be nested!
+ 
+ * If the person is married and if he lives in Finland, print the information in the following format:
+
+"""
+
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+        }
+    }
+
+if person['skills']:
+    print(person['skills'][(len(person['skills']))//2])
+if 'Python' in person['skills']:
+    print('this person has Python skills')
+
+
+if 'MongoDB' and 'Node' and 'React' in person['skills']:
+    print('He is a full stack developer')
+elif 'JavaScript' and 'React' in person['skills']:
+    print('He is a front end dev')
+elif 'Node' and 'Python' and 'MongoDB' in person['skills']:
+    print('He is a backend dev')
+else:
+    print('please put appropriate values in skills list')
