bfd2142bc63030480cc36a1f77cd6af0e5dd92fa
Gabuki <gabrielsomera06@gmail.com>
2026-05-14 22:04:04 -1000

    Add number guessing game implementation with difficulty selection and game logic

---
+++ projects/Day14/higher_lower_game.py
@@ -0,0 +1,20 @@
+import gamedata as gd
+import gameart
+
+# print_intro_art
+def showIntro():
+    print(gameart.logo)
+
+showIntro()
+
+# print_option_a
+# print_option_b
+
+# ask_user_which_option_has_more_followers
+
+# compare_user_option_with_other_option
+
+# compare_user_option_with_next_option_if_correct
+# follow_same_logic_at_start
+
+# end_game_if_wrong
